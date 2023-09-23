53bb9eb7940c8be4134a2b3a3de4581c402e94bc
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-09-23 21:39:58 +0300

    creating exp api.shopperProducts & using product scroller from PWA kit

--- app/pages/product-scroller/index.jsx
+++ app/pages/product-scroller/index.jsx
@@ -1,25 +1,36 @@
 import React, { useEffect } from "react";
 import ProductScrollerPWA from './../../components/product-scroller/index.jsx';
-import { Container, Box, Button, Input, Text } from '@chakra-ui/react'
+import { Container, Box, Button, Input, Stack } from '@chakra-ui/react'
 import { useState } from "react";
+// import { ShopperLogin, ShopperSearch, helpers } from "commerce-sdk-isomorphic";
+import PropTypes from 'prop-types'
 
-const ProductScroller = () => {
+// Constants
+import {
+    MAX_CACHE_AGE,
+    HOME_SHOP_PRODUCTS_CATEGORY_ID,
+    HOME_SHOP_PRODUCTS_LIMIT
+} from '../../constants'
+
+const ProductScroller = ({productSearchResult, isLoading, addedProducts, productListProp }) => {
 
     const [productID, setProductID] = useState("");
-    const [productList, setProductList] = useState([])
+    const [productList, setProductList] = useState([]);
 
-    const handleChangeProductID = (e) => {-
+    const handleChangeProductID = (e) => {
          setProductID(e.target.value)
     }
 
-
     //add new product to list
     const addProduct = () => {
+        console.log("idkidk")
         if(productID !== "" && !productList.includes(productID)) {
             const newArr = [...productList]
             newArr.push(productID)
             setProductList(newArr)
             setProductID("")
+            productListProp = newArr
+            console.log("productListProp addddd", productListProp)
         }
     }
 
@@ -30,10 +41,12 @@ const ProductScroller = () => {
             if(newArr[i] == removedProduct){
                 newArr.splice(i, 1);
                 setProductList(newArr)
+                productListProp = newArr
+                console.log("productListProp removvee", productListProp)
             }
         }
     }
-
+    
     return (
         <>
         <Container>
@@ -56,7 +69,7 @@ const ProductScroller = () => {
                         </Box> )
                     }
                     )}
-                        
+
                 </Box>
                 <Box display="flex" flexDirection="column">
                     <Button onClick={addProduct} mb={2}>Add Field</Button>
@@ -64,20 +77,81 @@ const ProductScroller = () => {
                 </Box>
             </Box>
 
-            <Box>
+            <Box mb={3}>
                 Product Scroller
-                
-                <ProductScrollerPWA />
-            {/* products=
-            isLoading= */}
-            </Box>
 
+                {addedProducts && (
+                <>
+                        <Stack pt={8} spacing={16}>
+                            <ProductScrollerPWA
+                                products={addedProducts.data}
+                                isLoading={isLoading}
+                            />
+                        </Stack>
+                    </>
+                )}
+            </Box>
 
         </Container>
 
 
         </>
     )
+}
+
+
+
+
+
+ProductScroller.getProps = async ({res, api, location, productListProp}) => {
+    if(res) {
+        res.set('Cache-Control', `max-age=${MAX_CACHE_AGE}`)
+    }
+
+    const urlParams = new URLSearchParams(location.search)
+
+    const productSearchResult = await api.shopperSearch.productSearch({
+        parameters: {
+            refine: [`cgid=${HOME_SHOP_PRODUCTS_CATEGORY_ID}`, 'htype=master'],
+            limit: HOME_SHOP_PRODUCTS_LIMIT
+        }
+    })
+
+    const addedProducts = await api.shopperProducts.getProducts({
+        parameters: {
+            // id: urlParams.get('pid') || productId,
+            ids: "25589419M,91736743M,25591410M"
+            // ids: productListProp.join(",")
+            // ids: productListProp ? `${productListProp}.join(",")` : ""
+        }
+    
+        // parameters: {
+        //     ids: ["91736743M", "25493613M"]
+        // }
+    })
+
+    return {productSearchResult, addedProducts, productListProp}
+}
+
+
+
+
+
+ProductScroller.propTypes = {
+    /**
+     * The search result object showing all the product hits, that belong
+     * in the supplied category.
+     */
+    productSearchResult: PropTypes.object,
+    /**
+     * The current state of `getProps` when running this value is `true`, otherwise it's
+     * `false`. (Provided internally)
+     */
+    isLoading: PropTypes.bool,
+
+    addedProducts: PropTypes.object,
+
+    productListProp: PropTypes.array
 }
 
 export default ProductScroller;
